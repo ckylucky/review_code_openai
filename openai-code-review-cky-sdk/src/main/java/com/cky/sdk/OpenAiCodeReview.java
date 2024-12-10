488b1c85a648fbc52537d6e62bd3cbf7c472fce1
lucky <1094568708@qq.com>
2024-12-10 10:16:35 +0800

    feat:v1

--- openai-code-review-cky-sdk/src/main/java/com/cky/sdk/OpenAiCodeReview.java
+++ openai-code-review-cky-sdk/src/main/java/com/cky/sdk/OpenAiCodeReview.java
@@ -1,5 +1,10 @@
 package com.cky.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @ClassName OpenAiCodeReview
  * @Description TODO
@@ -9,7 +14,25 @@ package com.cky.sdk;
  */
 
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("代码测试");
+        // 1. 代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode.toString());
     }
 }
