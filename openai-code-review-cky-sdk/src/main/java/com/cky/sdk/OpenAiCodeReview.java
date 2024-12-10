8ee7b67ef570d7e2b65794f19c583a534861c264
lucky <1094568708@qq.com>
2024-12-10 10:58:35 +0800

    feat:chatglm

--- openai-code-review-cky-sdk/src/main/java/com/cky/sdk/OpenAiCodeReview.java
+++ openai-code-review-cky-sdk/src/main/java/com/cky/sdk/OpenAiCodeReview.java
@@ -1,9 +1,16 @@
 package com.cky.sdk;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.alibaba.fastjson2.JSON;
+import com.cky.sdk.model.ChatCompletionRequest;
+import com.cky.sdk.model.ChatCompletionSyncResponse;
+import com.cky.sdk.model.Model;
+import com.cky.sdk.utils.BearerTokenUtils;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 /**
  * @ClassName OpenAiCodeReview
@@ -15,7 +22,8 @@ import java.io.InputStreamReader;
 
 public class OpenAiCodeReview {
     public static void main(String[] args) throws Exception {
-        System.out.println("代码测试");
+        System.out.println("测试执行");
+
         // 1. 代码检出
         ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
         processBuilder.directory(new File("."));
@@ -33,6 +41,61 @@ public class OpenAiCodeReview {
         int exitCode = process.waitFor();
         System.out.println("Exited with code:" + exitCode);
 
-        System.out.println("评审代码：" + diffCode.toString());
+        System.out.println("diff code：" + diffCode.toString());
+
+        // 2. chatglm 代码评审
+        String log = codeReview(diffCode.toString());
+        System.out.println("code review：" + log);
     }
+
+    private static String codeReview(String diffCode) throws Exception {
+
+        String apiKeySecret = "0c5a0365f98e66fafdd00b82ecd500d1.ybFuNCibDM2F7wUI";
+        String token = BearerTokenUtils.getToken(apiKeySecret);
+
+        URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer " + token);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        connection.setDoOutput(true);
+
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {
+            private static final long serialVersionUID = -7988151926241837899L;
+
+            {
+                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
+                add(new ChatCompletionRequest.Prompt("user", diffCode));
+            }
+        });
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
+            os.write(input);
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+
+        in.close();
+        connection.disconnect();
+
+        System.out.println("评审结果：" + content.toString());
+
+        ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
+        return response.getChoices().get(0).getMessage().getContent();
+    }
+
 }
