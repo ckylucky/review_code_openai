f9f415610b0e7d472a0e478be2fc088461d308f0
lucky <1094568708@qq.com>
2024-12-11 19:58:21 +0800

    feat:refactor v2.4

--- openai-code-review-cky-test/src/test/java/com/cky/Testapi.java
+++ openai-code-review-cky-test/src/test/java/com/cky/Testapi.java
@@ -13,7 +13,7 @@ public class Testapi {
 
     @Test
     void test(){
-        System.out.println(Integer.parseInt("23441245"));
+        System.out.println(Integer.parseInt("234412412"));
 
 
     }
