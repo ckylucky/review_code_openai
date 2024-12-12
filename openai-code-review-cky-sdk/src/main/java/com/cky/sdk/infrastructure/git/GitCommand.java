ee1e975c73d5f9a9afb30eea5e7fcd19be150d78
lucky <1094568708@qq.com>
2024-12-12 10:09:34 +0800

    feat:refactor v2.7

--- openai-code-review-cky-sdk/src/main/java/com/cky/sdk/infrastructure/git/GitCommand.java
+++ openai-code-review-cky-sdk/src/main/java/com/cky/sdk/infrastructure/git/GitCommand.java
@@ -94,7 +94,7 @@ public class GitCommand {
 
         logger.info("openai-code-review git commit and push done! {}", fileName);
 
-        return githubReviewLogUri + "/blob/master/" + dateFolderName + "/" + fileName;
+        return githubReviewLogUri + "/blob/main/" + dateFolderName + "/" + fileName;
     }
 
     public String getProject() {
