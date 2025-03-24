d27d186b03a8204eb911b3f61de1c86ba05608b1
lucky <1094568708@qq.com>
2025-03-24 11:34:36 +0800

    feat:master3

--- openai-code-review-cky-sdk/src/main/java/com/cky/sdk/infrastructure/openai/impl/Deepseek.java
+++ openai-code-review-cky-sdk/src/main/java/com/cky/sdk/infrastructure/openai/impl/Deepseek.java
@@ -67,9 +67,13 @@ public class Deepseek implements IOpenAI {
         if (requestDTO.getMessages() == null || requestDTO.getMessages().isEmpty()) {
             throw new IllegalArgumentException("Messages cannot be null or empty");
         }
+
         // 🥡 把小吃摊的饭盒装进高级餐盘
         ChatCompletionSyncResponseDTO.Choice choice = new ChatCompletionSyncResponseDTO.Choice();
-        JSONObject message = ollamaResponse.getJSONObject("message"); // ▶️ 响应结构不同
+        JSONObject message = ollamaResponse.getJSONObject("message"); // ▶️ 响应结构不同''
+        if (message.getString("role").isEmpty() ||message.getString("content").isEmpty()) {
+            throw new IllegalArgumentException("role cannot be null or empty");
+        }
         choice.getMessage().setRole(message.getString("role"));
         choice.getMessage().setContent(message.getString("content"));
 
